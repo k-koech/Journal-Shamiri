9c32d1a4e3b509bf513e953b8e0dd1ce73694761
k-koech <koechkelvin97@gmail.com>
2024-07-05 21:32:34 +0300

    create urls.py in journal and linked it to the main app

--- backend/journal/views.py
+++ backend/journal/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework import status
+from django.http import JsonResponse
+
 
 # Create your views here.
+def home(request):
+    # return Response({'info': 'Journal Home'}, status=200)
+    return JsonResponse({"error":"User does not exist, please register with google first!"}, status=status.HTTP_406_NOT_ACCEPTABLE)
+
