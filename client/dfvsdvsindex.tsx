c66b9a4c6e8e2b6062d116c63a5243f0bf23a803
k-koech <koechkelvin97@gmail.com>
2024-07-07 07:08:36 +0300

    change frontend folder to client/create folder structure for react native app

---
+++ client/dfvsdvsindex.tsx
@@ -0,0 +1,14 @@
+
+import 'expo-router/entry'
+// import { View, Text } from 'react-native'
+// import React from 'react'
+
+// const index = () => {
+//   return (
+//     <View>
+//       <Text>index</Text>
+//     </View>
+//   )
+// }
+
+// export default index
