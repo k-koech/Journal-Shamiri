0dcfc548ac7d9d1b5c6bb63b3a3bcebb866f04ca
k-koech <koechkelvin97@gmail.com>
2024-07-07 12:09:28 +0300

    Create the first navigation

--- client/app/index.tsx
+++ client/app/index.tsx
@@ -1,10 +1,27 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import AuthScreen from './screens/AuthScreen';
+import HomeScreen from './screens/HomeScreen';
+import JournalScreen from './screens/JournalScreen';
+import JournalDetailScreen from './screens/JournalDetailScreen';
+import ProfileScreen from './screens/ProfileScreen';
+import SummaryScreen from './screens/SummaryScreen';
+
+
+const Stack = createStackNavigator();
 
 export default function index() {
   return (
-    <View>
-      <Text className='Bg bg-red-200'>index</Text>
-    </View>
+    <NavigationContainer independent={true}>
+    <Stack.Navigator initialRouteName="Auth">
+      <Stack.Screen name="Auth" component={AuthScreen} />
+      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="Journal" component={JournalScreen} />
+      <Stack.Screen name="JournalDetail" component={JournalDetailScreen} />
+      <Stack.Screen name="Profile" component={ProfileScreen} />
+      <Stack.Screen name="Summary" component={SummaryScreen} />
+    </Stack.Navigator>
+  </NavigationContainer>
   )
 }
